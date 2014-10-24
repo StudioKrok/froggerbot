e5132cdff6c8e29e928a9f12bf8b18a830365b27
Agares <ekeisco@gmail.com>
2014-10-24 14:25:43 -0500

    implements simple keyMapper

---
+++ public/keyMapper.js
@@ -0,0 +1,40 @@
+var KEYS = {
+  LEFT:1,
+  UP:2,
+  RIGHT:4,
+  DOWN:8,
+  SPACE:16,
+  ENTER:32,
+}
+var keyMapper = (function(){
+  var keyMap = 0;
+  var keys = {
+    '37': KEYS.LEFT,
+    '38': KEYS.UP,
+    '39': KEYS.RIGHT,
+    '40': KEYS.DOWN,
+    '32': KEYS.SPACE,
+    '13': KEYS.ENTER 
+  }
+  var listener = function(){};
+
+  document.addEventListener('keydown', function(e){
+    var key = e.keyCode|| e.which;
+    if(keys[key]){
+      listener(keys[key]);
+      e.preventDefault();
+    }
+  });
+
+  document.addEventListener('keyup',  function(e){
+    var key = e.keyCode ? e.keyCode : e.which;
+    if(keyMap&keys[key]){
+      e.preventDefault();
+    }
+  });
+  return{
+    setKeyListener: function(newListener){
+      listener = newListener;
+    }
+  }  
+})();
--- public/main.js
+++ public/main.js
@@ -2,4 +2,19 @@ var canvas = document.getElementById('myCanvas');
 
 var context = canvas.getContext('2d');
 
-context.fillRect(10,10,20,20);
+context.fillRect(10,10,200,200);
+
+var colors = {
+  '1': 'green',
+  '2': 'red',
+  '4': 'yellow',
+  '8': 'black',
+  '16': 'magenta',
+  '32': 'white'
+};
+
+keyMapper.setKeyListener(function(key){
+  console.log(key);
+  context.fillStyle = colors[key];
+  context.fillRect(14,14,200,200);
+});
